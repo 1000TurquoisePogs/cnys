748dddabd4e3ae072f7bb44e3a459b7e0e901b45
1000TurquoisePogs <sgrady@rocketsoftware.com>
2023-12-01 09:28:36 -0500

    Return event emitter that gives upload event. Fix remote upload failing due to typo on conn2. Make logging 80 chars wide
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const EventEmitter = require('events').EventEmitter;
 const { launchServer, launchWatcher } = require('./lib'),
   { debug } = require('./lib/display'),
   launchCommand = require('./commands'),
   _config = require('./config');
 
 module.exports = cfg => {
+  const emitter = new EventEmitter();
+  cfg.emitter = emitter;
+
   const config = { ..._config, ...cfg };
   debug(config.debug, config);
   const server = launchServer(config);
   launchWatcher(server, config);
+  return emitter;
 };
